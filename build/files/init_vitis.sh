6cd29c5d29b79f30d8e41f332f0a024fbc250dd5
Qianyu Cheng <qianyu.cheng.chn@gmail.com>
2024-01-23 21:17:11 +0800

    Update init_vitis.sh

--- build/files/init_vitis.sh
+++ build/files/init_vitis.sh
@@ -1,15 +1,4 @@
-#!/bin/bash -eu
-
-##
-## @file        init_vitis.sh
-## @brief       Script to initialize Vitis
-## @author      Keitetsu
-## @date        2020/04/13
-## @copyright   Copyright (c) 2020 Keitetsu
-## @par         License
-##              This software is released under the MIT License.
-##
-
-if [ -f /opt/Xilinx/Vitis/2021.2/settings64.sh ]; then
-    . /opt/Xilinx/Vitis/2021.2/settings64.sh
+#!/bin/bash
+if [ -f /tools/Xilinx/Vitis/${XILINX_VERSION}/settings64.sh ]; then
+    . /tools/Xilinx/Vitis/${XILINX_VERSION}/settings64.sh
 fi

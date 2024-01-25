6e90dd31c548a5a4b280c502a6c32463b824dd26
Qianyu Cheng <qianyu.cheng.chn@gmail.com>
2024-01-25 19:37:11 +0800

    Update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-XILINX_VERSION="2022.2"
 docker build \
-    --tag ghcr.io/matsubarai/Vitis-Docker/vitis:${XILINX_VERSION} \
+    --tag ghcr.io/matsubarai/Vitis-Docker/vitis:base \
     .
 
